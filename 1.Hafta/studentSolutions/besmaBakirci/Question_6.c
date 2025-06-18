75f23c8c52a97c943cbb59ceee8b5e16671b9229
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-18 23:06:43 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_6.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_6.c
@@ -4,12 +4,31 @@
 ///  ve bu for döngüsü first in last out şeklinde yazılacak yani arrayin 0.
 ///  indeksi size 5 ise 4. indekse sahip olacak 
 
-#include < stdio.h> 
+#include <stdio.h> 
 
 int main(){
     int size ;
+
     printf("Please enter the size of array : ");
     scanf("%d", &size);
-    int arr [size] ;
 
-}
+    int arr[size];
+
+    for(int i = 0; i<size ; i++) {
+        printf("Please enter element %d: ", i +1 );
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < (size / 2) ; i++){
+    int temporary = arr[i];
+    arr[i] = arr[(size-1)-i];
+    arr[(size-1)-i] = temporary;
+    }
+
+    for (int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+} 

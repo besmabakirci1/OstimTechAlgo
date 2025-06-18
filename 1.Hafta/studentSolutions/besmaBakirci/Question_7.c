c865083a422ad5bd70a208ef88467d47d76f0938
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-18 23:15:18 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_7.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_7.c
@@ -1,1 +1,3 @@
 // Bir dizinin palindrom olup olmadığını kontrol edin
+// Palindrom dizi nedir ? : tersten okunuşu da düz okunuşu ile aynı olan dizidir.
+/// Çözüm Yöntemim : Kullanıcıdan bir array alınır ve bu arrayin 

b8a9d5b09ca5ffe207861c1315e514318493673f
Poyraz <poyrazavsever@hotmail.com>
2025-06-19 21:11:02 +0300

    feat: add function to count numbers of a specific number in an array

--- 1.Hafta/studentSolutions/poyraz/4.js
+++ 1.Hafta/studentSolutions/poyraz/4.js
@@ -1,1 +1,17 @@
 // Bir dizide belirli bir sayının kaç kere geçtiğini bulun.
+
+const countNumbers = (arr, target) => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === target) {
+        count++;
+      }
+    }
+    return count;
+  };
+  
+  const numbers = [2, 5, 2, 7123, 2, 23, 42, 653, 86];
+  const target = 2;
+  
+  console.log(`${target} sayısı dizide ${countNumbers(numbers, target)} kere geçiyor hocam.`);
+  

7147a9b701af53808a441af9693d6ed304b3b763
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-19 17:41:19 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_8.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_8.c
@@ -1,1 +1,29 @@
 // Verilen bir dizide yinelenen (duplicate) elemanları bulun
+/// Çözüm Yöntemim: kullanıcıdan bir dize alınır ve 
+// birinci indexteki eleman tutularak tüm dizideki elemanlar ile eşit olup olmadığı karşılaştırılır ve 
+// sonuç itibarı ile bu tüm elemanlar için tekrar edilir ve sonunda dublicate sayılar yazılır 
+
+#include <stdio.h>
+
+int main(void){
+    int n ;
+    printf("Please enter the size of array: "); 
+    scanf("%d",&n);
+    int arr[n];
+
+    printf("Please enter the array's elements: \n");
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("Duplicate Numbers:\n");
+
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] == arr[j]){
+                printf("%d\n", arr[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}

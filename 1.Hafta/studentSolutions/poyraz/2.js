eb988e0b8f0f45715f3a62a37c08b0c585fb1918
Poyraz <poyrazavsever@hotmail.com>
2025-06-19 21:07:43 +0300

    feat: done second question

--- 1.Hafta/studentSolutions/poyraz/2.js
+++ 1.Hafta/studentSolutions/poyraz/2.js
@@ -1,1 +1,11 @@
 // Bir dizinin tüm elemanlarını ekrana yazdıran bir fonksiyon yazın.
+
+const printArray = (arr) => {
+    arr.forEach((element) => { // foreach ile döndüm, alternatifinde length ile dönülüp yazdırılabilir.
+      console.log(element);
+    });
+  };
+  
+  const numbers = [1, 2, 3, 4, 5];
+  printArray(numbers);
+  

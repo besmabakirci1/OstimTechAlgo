4e923c83ea4382bf768a25c55a8c413e0886412c
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-19 22:06:55 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_12.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_12.c
@@ -1,2 +1,25 @@
-// Verilen bir diziden negatif sayıları çıkarıp, sadece pozitifleri içeren yeni bir dizi oluşturun.
-/// Çözüm Yöntemim: 
+// Verilen bir diziden negatif sayıları çıkarıp, 
+// sadece pozitifleri içeren yeni bir dizi oluşturun.
+#include <stdio.h>
+#define SIZE 5
+
+int main(void){
+    int arr[SIZE] = {-12, -4, 5 ,9 ,10};
+    int positiveArr[SIZE];
+    int positiveCount = 0;
+
+
+    for(int i = 0; i < SIZE; i++){
+        if (arr[i] > 0 ){
+            positiveArr[positiveCount] = arr[i];
+            positiveCount++;
+        }
+    }
+
+    printf("Positive NUmbers %d: ", positiveCount);
+    for(int i = 0; i < positiveCount; i++){
+        printf("\n %d", positiveArr[i]);
+    }
+
+    return 0;
+ }

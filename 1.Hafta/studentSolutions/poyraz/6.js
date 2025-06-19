9bcd4a14e9707bc90dbb111474a52802f0201173
Poyraz <poyrazavsever@hotmail.com>
2025-06-19 21:17:47 +0300

    feat: done 6js

--- 1.Hafta/studentSolutions/poyraz/6.js
+++ 1.Hafta/studentSolutions/poyraz/6.js
@@ -1,1 +1,22 @@
 // Bir dizi verildiğinde, dizideki tüm elemanları ters çevirin (reverse).
+
+const reverseArray = (arr) => {
+    return arr.slice().reverse();
+  };
+  
+  const numbers = [1, 2, 3, 4, 5];
+  console.log("Reversed array:", reverseArray(numbers));
+
+
+/*
+const reverseArray = (arr) => {
+  const result = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  return result;
+};
+
+const numbers = [1, 2, 3, 4, 5];
+console.log("Reversed array:", reverseArray(numbers));
+*/

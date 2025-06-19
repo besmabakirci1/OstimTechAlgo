ea0e385237339670f9006f827333c8f3dea8c770
Poyraz <poyrazavsever@hotmail.com>
2025-06-19 21:08:42 +0300

    feat: done third question

--- 1.Hafta/studentSolutions/poyraz/3.js
+++ 1.Hafta/studentSolutions/poyraz/3.js
@@ -1,1 +1,13 @@
 // Bir dizideki tüm elemanların toplamını hesaplayın.
+
+const sumElements = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
+  };
+  
+  const numbers = [5, 10, 15, 20];
+  console.log("Toplam:", sumElements(numbers));
+  

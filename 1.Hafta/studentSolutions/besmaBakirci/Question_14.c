9a5af1a06331bd4fd1599f5302f4bca17cdcaf6b
Basma Bakirci <144848782+besmabakirci1@users.noreply.github.com>
2025-06-20 23:06:01 +0300

    :)

--- 1.Hafta/studentSolutions/besmaBakirci/Question_14.c
+++ 1.Hafta/studentSolutions/besmaBakirci/Question_14.c
@@ -1,1 +1,9 @@
 // Verilen bir dizideki ardışık olarak artan en uzun alt diziyi bulun.
+
+#include <stdio.h>
+#define SIZE 7
+
+int main(void){
+    int arr[SIZE] = {-12, -8, -4, 0, 4, 8, 12};
+    int positiveArr[SIZE];
+}
